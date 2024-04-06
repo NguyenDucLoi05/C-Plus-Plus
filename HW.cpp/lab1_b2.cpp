8b06cb87e846c251720f8f331603c2325714e1e5
DucLoi <nguyenducloi1105@gmail.com>
2024-04-06 20:37:03 +0700

    cpp

--- HW.cpp/lab1_b2.cpp
+++ HW.cpp/lab1_b2.cpp
@@ -3,19 +3,29 @@ using namespace std;
 
 int main()
 {
-  int n, sum(0);
+  int n, sum(0), tong(0);
   cout << "Hay nhap so n: " << endl;
   cin >> n;
   if (n > 0)
   {
     {
-
       for (int i = 0; i <= n; i++)
         sum += i;
     }
     cout << sum;
   }
   else
-    cout << "Nhap sai hay nhap lai!";
+    cout << "Nhap sai hay nhap lai!" << endl;
+// cach khac
+  do
+  {
+    {
+      for (int j = 0; j <= n; j++)
+        tong += j;
+    }
+    cout << tong;
+    break;
+  }
+  while (n>0);
   return 0;
 }
